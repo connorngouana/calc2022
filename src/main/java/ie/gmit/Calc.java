02ea5150b08415031bdd4685fe26ab63e0f0097c
connorngouana <connorngouana@gmail.com>
2022-10-19 13:39:53 +0100

    added a method to the code called add

--- src/main/java/ie/gmit/Calc.java
+++ src/main/java/ie/gmit/Calc.java
@@ -1,9 +1,25 @@
 package ie.gmit;
-
+import java.util.Scanner;
 public class Calc
 {
     public static void main(String[] args)
     {
+        add();
+    }
+
+    private static void add()
+    {
+        System.out.println("Please your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstName= inputs.nextInt();
+
+        System.out.println("Please your Second number: ");
+        int secondName= inputs.nextInt();
+
+        int total = firstName + secondName;
+        System.out.println("The total is: " + total);
 
     }
+
+
 }
